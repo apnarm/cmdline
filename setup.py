8c81cdfc9b760bd537b8a9475adac778e5b54f3b
David Nugent <david.nugent@news.com.au>
2021-01-20 09:14:19 +1100

    more complete setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ except ImportError:
 setup(
     name='cmdline',
     version='0.1.0',
+    description='mini partial command line parser',
+    author='David Nugent',
+    author_email='david.nugent@news.com.au',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=[
         'cmdline',
     ],
