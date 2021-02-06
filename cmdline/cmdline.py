b41115ae6f0511db1e761f19d8b96b5792368e30
David Nugent <david.nugent@news.com.au>
2021-02-07 10:55:09 +1100

    api change - add results dict param and return, params require kwargs

--- cmdline/cmdline.py
+++ cmdline/cmdline.py
@@ -6,7 +6,7 @@ Purpose: add switches with actions to an existing command line processor
 Accepts an array of options with members as follows:
 """
 import sys
-from typing import NamedTuple, Sequence, Callable
+from typing import NamedTuple, Sequence, Dict, Any
 from contextlib import contextmanager
 
 
@@ -23,25 +23,41 @@ class Option(NamedTuple):
     short: str
     long: str
     has_arg: bool = False
+    fn: Any = None
 
 
-def system_args(options: Sequence[Option], process: Callable, error: Callable = None):
-    prog = sys.argv[0]
-    args = sys.argv[1:]
-    process(None, prog, args)
-    args = cmdline_args(args, options, process, error)
+def system_args(options: Sequence[Option], *, process: callable = None,
+                error: callable = None, results: dict = None) -> Dict:
+    """
+    Parse the command line args and partially process them
+    :param options: sequence of options to parse
+    :param process: process function
+    :param error: error function
+    :param results: optional dict to contain results (alternative to process callable)
+    :return: parsed results, remaining unprocessed arguments
+    """
+    prog, args = sys.argv[0], sys.argv[1:]
+    if results is None:
+        results = {}
+    if process:
+        process(None, prog, args)
+    else:
+        results.setdefault('prog', prog)
+    results, args = cmdline_args(args, options, process=process, error=error, results=results)
     sys.argv = [prog] + list(args)
+    return results
 
 
-def cmdline_args(argv: Sequence[str], options: Sequence[Option], process: Callable, error: Callable = None) \
-        -> Sequence[str]:
+def cmdline_args(argv: Sequence[str], options: Sequence[Option], *, process: callable = None,
+                 error: callable = None, results: dict = None) -> (Dict, Sequence[str]):
     """
     Take an array of command line args, process them
     :param argv: argument array
     :param options: sequence of options to parse
     :param process: process function
     :param error: error function
-    :return: remaining unprocessed arguments
+    :param results: optional dict to contain results (alternative to process callable)
+    :return: parsed results, remaining unprocessed arguments
     """
 
     def select_option(short_opt, long_opt):
@@ -62,9 +78,21 @@ def cmdline_args(argv: Sequence[str], options: Sequence[Option], process: Callab
                     error(f"unknown long option  '--{long_opt}'")
         return selected_option
 
-    index = 0
-    skip_count = 0
+    def dispatch_option(_option: Option, _opt: str, _args):
+        if _option.fn is not None:
+            return _option.fn(_option, _opt, _args) if callable(_option.fn) else _option.fn
+        if process:
+            tmp = process(_option, _opt, _args)
+            if tmp is not None:
+                return tmp
+        return _args if _option.has_arg else True
+
+    if results is None:
+        results = dict()
+
+    index = skip_count = 0
     saved_args = []
+
     for index, arg in enumerate(argv):
         if skip_count:
             skip_count -= 1
@@ -82,7 +110,7 @@ def cmdline_args(argv: Sequence[str], options: Sequence[Option], process: Callab
                     else:
                         skip_count += 1
                         args = argv[index + skip_count]
-                process(option, longopt, args)
+                results[option.long] = dispatch_option(option, longopt, args)
         elif arg.startswith('-'):
             skip_count = 0
             for opt in arg[1:]:
@@ -92,24 +120,39 @@ def cmdline_args(argv: Sequence[str], options: Sequence[Option], process: Callab
                 else:
                     if option.has_arg:
                         skip_count += 1
-                    process(option, opt, argv[index + skip_count] if option.has_arg else None)
+                    args = argv[index + skip_count] if option.has_arg else None
+                    results[option.long] = dispatch_option(option, opt, args)
         else:
             break
-    return saved_args + argv[index + skip_count:]
+    return results, saved_args + [arg for arg in argv[index + skip_count:]]
 
 
 @contextmanager
 def redirect_stdout(stream):
-    stdout = sys.stdout
-    sys.stdout = stream
-    yield
-    sys.stdout.flush()
-    sys.stdout = stdout
+    stdout, sys.stdout = sys.stdout, stream
+    try:
+        yield
+    finally:
+        sys.stdout.flush()
+        sys.stdout = stdout
 
 
 def add_pythonpath(*args, prepend=True):
+    """
+    Add a path to python search path, avoid duplication
+    :param args: paths to add to python path
+    :param prepend: if true, paths are inserted at the start, else appended
+    :return:
+
+    This relies on Python 3.6+ insertion order of builtin dict
+    """
+    # prepend args to python path if prepend
     pythonpath = {str(p): None for p in args} if prepend else {}
+    # add the python path
     pythonpath.update({p: None for p in sys.path})
+    # append args to end unless prepend
     pythonpath.update({} if prepend else {str(p): None for p in args})
+    # convert back to a list
     sys.path = list(pythonpath.keys())
+    # end return it
     return sys.path
--- tests/test_cmd_args.py
+++ tests/test_cmd_args.py
@@ -15,6 +15,32 @@ def options():
         Option('v', 'verbose'),
     ]
 
+@pytest.fixture
+def options_fn():
+    return [
+        Option('a', 'always', fn=True),
+        Option('b', 'book', has_arg=True),
+        Option('c', 'calendar', fn=False),
+        Option('d', 'date', has_arg=True),
+        Option('s', 'sign', has_arg=True),
+        Option('p', 'peak', has_arg=True),
+        Option('q', 'quiet', fn=True),
+        Option('v', 'verbose', fn=True),
+    ]
+
+@pytest.fixture
+def defaults():
+    return dict(
+        always=False,
+        book='default book',
+        calendar=True,
+        date='2000-01-02',
+        sign='ares',
+        peak='0db',
+        quiet=False,
+        verbose=False,
+    )
+
 class BoundTest:
 
     def __init__(self):
@@ -31,19 +57,19 @@ def test_cmd_args_tuple(options):
     argument_list = ('--book', 'hello world', '--calendar', '-ad', '2021-02-14', 'the', 'rest', '--help')
     instance = BoundTest()
 
-    the_rest = cmdline_args(argument_list, options, instance.process, instance.error)
+    results, the_rest = cmdline_args(argument_list, options, process=instance.process, error=instance.error)
 
     assert (options[1], 'book', 'hello world') in instance.result_list
     assert len(instance.result_list) == 4
-    assert isinstance(the_rest, tuple)
-    assert the_rest == ('the', 'rest', '--help')
+    assert isinstance(the_rest, list)
+    assert the_rest == ['the', 'rest', '--help']
 
 
 def test_cmd_args_list(options):
     argument_list = ['--sign', 'leo', '--peak=-62db', '-ad', '2021-02-14', '--verb', '-q', 'and', 'the', 'rest', '--help']
     instance = BoundTest()
 
-    the_rest = cmdline_args(argument_list, options, instance.process, instance.error)
+    results, the_rest = cmdline_args(argument_list, options, process=instance.process, error=instance.error)
 
     assert (options[4], 'sign', 'leo') in instance.result_list
     assert (options[5], 'peak', '-62db') in instance.result_list
@@ -51,18 +77,62 @@ def test_cmd_args_list(options):
     assert isinstance(the_rest, list)
     assert the_rest == ['and', 'the', 'rest', '--help']
 
+    assert isinstance(results, dict)
+
+
+def test_cmd_args_results(options_fn, defaults):
+    argument_list = ['--sign', 'leo', '--peak=-62db', '-ad', '2021-02-14', '--verb', '-q',
+                     'and', 'the', 'rest', '--help']
+    instance = BoundTest()
+
+    results, the_rest = cmdline_args(argument_list, options_fn, error=instance.error)
+
+    assert the_rest == ['and', 'the', 'rest', '--help']
+    assert results == dict(sign='leo', peak='-62db', always=True, date='2021-02-14', verbose=True, quiet=True)
+
+    results, the_rest = cmdline_args(argument_list, options_fn, error=instance.error, results=defaults.copy())
+
+    assert results == dict(book='default book', calendar=True, sign='leo', peak='-62db', always=True,
+                           date='2021-02-14', verbose=True, quiet=True)
+
 
 def test_system_cmd(options):
     import sys
-    argument_list = ('/bin/myscript', '--sign', 'leo', '--peak', '-62db', '-ad', '2021-02-14', '--verb', '-q', 'and', 'the', 'rest', '--help')
+    argument_list = ('/bin/myscript', '--sign', 'leo', '--peak', '-62db', '-ad', '2021-02-14', '--verb', '-q',
+                     'and', 'the', 'rest', '--help')
     instance = BoundTest()
 
     sys.argv = argument_list
-    system_args(options, instance.process, instance.error)
+    results = system_args(options, process=instance.process, error=instance.error)
 
     assert (options[4], 'sign', 'leo') in instance.result_list
     assert (options[5], 'peak', '-62db') in instance.result_list
     assert len(instance.result_list) == 7
-    assert isinstance(sys.argv, tuple)
-    assert sys.argv == ('/bin/myscript', 'and', 'the', 'rest', '--help')
+    assert isinstance(sys.argv, list)
+    assert sys.argv == ['/bin/myscript', 'and', 'the', 'rest', '--help']
 
+    assert isinstance(results, dict)
+
+
+def test_system_cmd_results(options_fn, defaults):
+    import sys
+    argument_list = ('/bin/myscript', '--sign', 'leo', '--peak', '-62db', '-ad', '2021-02-14', '--verb', '-q',
+                     'and', 'the', 'rest', '--help')
+    instance = BoundTest()
+
+    sys.argv = argument_list
+    results = system_args(options_fn, error=instance.error)
+
+    assert isinstance(results, dict)
+    assert sys.argv == ['/bin/myscript', 'and', 'the', 'rest', '--help']
+
+    assert isinstance(results, dict)
+    assert results == dict(prog='/bin/myscript', sign='leo', peak='-62db', always=True, date='2021-02-14',
+                           verbose=True, quiet=True)
+
+    sys.argv = argument_list
+    results = system_args(options_fn, error=instance.error, results=defaults.copy())
+
+    assert sys.argv == ['/bin/myscript', 'and', 'the', 'rest', '--help']
+    assert results == dict(prog='/bin/myscript', book='default book', calendar=True, sign='leo', peak='-62db',
+                           always=True, date='2021-02-14', verbose=True, quiet=True)
